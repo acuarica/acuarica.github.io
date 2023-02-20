8c900b5954629c6cecbb38b6b0c9106ede228ab6
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-02-20 23:24:03 +0100

    Set up tailwind & sitemap integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()],
+    integrations: [
+        // https://docs.astro.build/en/guides/integrations-guide/mdx/
+        mdx(),
+
+        // https://docs.astro.build/en/guides/integrations-guide/tailwind/
+        tailwind(),
+
+        // https://docs.astro.build/en/guides/integrations-guide/sitemap/
+        // See `site` field below
+        sitemap(),
+    ],
+
+    site: 'https://acuarica.github.io',
 });
