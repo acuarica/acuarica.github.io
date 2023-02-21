08a49fbc014807c9c0eb94270d7b3eac28eb6425
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-02-21 05:33:43 +0100

    Add education and experience

---
+++ src/utils/index.ts
@@ -0,0 +1,18 @@
+import { CollectionEntry, getCollection } from "astro:content";
+
+type Entry = any extends CollectionEntry<infer R> ? R : never;
+
+export async function getRenderedCollection<C extends Entry>(
+    collection: C
+) {
+    return await Promise.all(
+        (await getCollection(collection)).map(async (entry) => ({
+            ...entry,
+            Content: (await entry.render()).Content,
+        }))
+    );
+}
+
+export function sort<E extends { data: { date: { date: Date } } }>(collection: E[]): E[] {
+    return collection.sort((lhs, rhs) => rhs.data.date.date.getTime() - lhs.data.date.date.getTime());
+}
