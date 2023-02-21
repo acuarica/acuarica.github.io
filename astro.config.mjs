f69ef5bc14074c05abab2d14cf448213bbfd7044
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-02-21 05:30:01 +0100

    Set up `base` path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,27 @@
 import { defineConfig } from 'astro/config';
+
+// https://docs.astro.build/en/guides/integrations-guide/mdx/
 import mdx from '@astrojs/mdx';
+
+// https://docs.astro.build/en/guides/integrations-guide/tailwind/
 import tailwind from '@astrojs/tailwind';
+
+// https://docs.astro.build/en/guides/integrations-guide/sitemap/
+// See `site` field below
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        // https://docs.astro.build/en/guides/integrations-guide/mdx/
-        mdx(),
-
-        // https://docs.astro.build/en/guides/integrations-guide/tailwind/
-        tailwind(),
-
-        // https://docs.astro.build/en/guides/integrations-guide/sitemap/
-        // See `site` field below
-        sitemap(),
-    ],
-
+    // https://docs.astro.build/en/reference/configuration-reference/#site
     site: 'https://acuarica.github.io',
+
+    // https://docs.astro.build/en/reference/configuration-reference/#base
+    base: '/dev',
+
+    // https://docs.astro.build/en/reference/configuration-reference/#integrations
+    integrations: [
+        mdx(),
+        tailwind(),
+        sitemap()
+    ],
 });
