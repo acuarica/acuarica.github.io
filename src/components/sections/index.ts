bc951ccf4654c75b31299f04a18625473891eba3
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-02-24 07:51:23 +0100

    Divide sections using css instead of `Divider`

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,6 +1,8 @@
 
+import Section from "./Section.astro";
+import Hero from "./Hero.astro";
 import Projects from "./Projects.astro";
 import Posts from "./Posts.astro";
 import Publications from "./Publications.astro";
 
-export default { Projects, Posts, Publications };
+export default { Section, Hero, Projects, Posts, Publications };
