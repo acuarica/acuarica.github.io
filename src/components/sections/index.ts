6c26c5f86f1c07285366dc93df93c5e43474771d
Luis Mastrangelo <luismastrangelo@gmail.com>
2023-02-24 02:44:46 +0100

    Add projs, posts and pubs to tags page

---
+++ src/components/sections/index.ts
@@ -0,0 +1,6 @@
+
+import Projects from "./Projects.astro";
+import Posts from "./Posts.astro";
+import Publications from "./Publications.astro";
+
+export default { Projects, Posts, Publications };
--- src/utils/content.ts
+++ src/utils/content.ts
@@ -2,15 +2,27 @@ import { CollectionEntry, getCollection } from "astro:content";
 
 type Entry = any extends CollectionEntry<infer R> ? R : never;
 
-export async function getRenderedCollection<C extends Entry>(
-    collection: C
-) {
+type Render = {
+    Content: import('astro').MarkdownInstance<{}>['Content'];
+    headings: import('astro').MarkdownHeading[];
+    remarkPluginFrontmatter: Record<string, any>;
+};
+
+export async function renderCollection<C extends Entry>(
+    collection: CollectionEntry<C>[]
+): Promise<(CollectionEntry<C> & Render)[]> {
     return await Promise.all(
-        (await getCollection(collection)).map(async entry => ({
+        collection.map(async entry => ({
             ...entry,
             ...await entry.render(),
         }))
     );
+}
+
+export async function getRenderedCollection<C extends Entry>(
+    collection: C
+): Promise<(CollectionEntry<C> & Render)[]> {
+    return await renderCollection(await getCollection(collection));
 }
 
 export function sort<E extends { data: { date: { date: Date } } }>(collection: E[]): E[] {
